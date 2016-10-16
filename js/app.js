2a9b19b24eb9903206a9a5ee1f6a1446d0cbe7cd
sim0nklein <simklein@gmx.net>
2016-10-16 21:26:55 +0200

    java scroll

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 $('.parallax-window').parallax({ imageSrc: 'images/backpar.jpg' });
 
 
-$('.panel').on('shown.bs.collapse', function(e) {
-    $('body').scrollspy({ target: '#panel01' })
+// $('.panel').on('shown.bs.collapse', function(e) {
 
-    console.log(e);
+//     console.log(e);
 
 
-})
+// });
+$(function() {
+    $('#accordion').on('shown.bs.collapse', function(e) {
+        var offset = $(this).find('.collapse.in').prev('.panel-heading');
+        if (offset) {
+            $('html,body').animate({
+                scrollTop: $(offset).offset().top - 50
+            }, 500);
+        }
+    });
+});
 
 // $('body').parallax({ imageSrc: 'images/delf.png' });
